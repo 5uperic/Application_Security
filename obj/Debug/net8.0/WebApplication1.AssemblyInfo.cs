4f516d7a4d49f33fbe96ddb4ae4d1b432c5fb2cc
andric <you@example.com>
2025-02-14 07:56:23 +0800

    what the sigma

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13560d549c3aa6b5a5650ef4746582c766936247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f12d654db79bfd451c24e6a0ed2919f2ab37ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
